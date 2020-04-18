faecfa8b7adaae17240eec28a10e4ec3ddbfde77
YouuInKeun <keun0390@naver.com>
2020-04-18 16:31:33 +0900

    [ADD] initial commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\keun0\onedrive\바탕 화면\멋사(장고)\멋쟁이사자처럼 8기 운영진\likelion-blog-class\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
